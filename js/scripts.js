ca0189dab21280f6c45f241c2516d7e9ea0fb289
Robert Bunch <rob@digitalcrafts.com>
2017-10-10 10:23:30 -0400

    init repo

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+// Add ready listener to the document.
+// I.e., JavaScript, wait until the DOM is fully loaded before proceeding to this code
+$(document).ready(function(){
+	console.log("Sanity Check!!")
+});
